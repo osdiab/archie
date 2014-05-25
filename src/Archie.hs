b975c67d8fad78c3ab848a85dcbe0530c8d6af8a
Alexander Atallah <aatallah@stanford.edu>
2014-05-25 16:02:52 -0700

    testing inherit, added comments

--- src/Archie.hs
+++ src/Archie.hs
@@ -1,3 +1,4 @@
+-- TODO remove these somehow
 {-# LANGUAGE GeneralizedNewtypeDeriving, TypeSynonymInstances, FlexibleInstances #-}
 
 module Archie (archie) where
@@ -15,8 +16,9 @@ class JSVal a where
   valueJS :: a -> JSValue
 
 instance Val Selector where
-  value a = value $ Literal $ Text.pack "inherit"
+  value _ = value $ Literal $ Text.pack "inherit"
 
+-- TODO replace the dummy "attr()" with a type for JS conversion
 instance JSVal Reference where
   valueJS a = JSValue { unJSValue = show a ++ ".attr()" }
 
--- test/Test.hs
+++ test/Test.hs
@@ -4,17 +4,24 @@ import Test.Hspec
 import Test.QuickCheck
 
 import Archie
+import Clay
+import Clay.Selector
+import qualified Data.Text as Text
+
+instance Eq Value where
+  (==) (Value (Prefixed a)) (Value (Prefixed b)) = a == b
+  (==) (Value (Plain a)) (Value (Plain b)) = a == b
+  (==) (Value _) (Value _) = False
+  (/=) (Value (Prefixed a)) (Value (Prefixed b)) = a /= b
+  (/=) (Value (Plain a)) (Value (Plain b)) = a /= b
+  (/=) (Value _) (Value _) = True
 
 main :: IO ()
-main = hspec $ describe "Testing Lab 3" $ do
+main = hspec $ describe "Testing Archie" $ do
 
   -- HUnit/HSpec  tests.
-  describe "main" $ do
-    it "is main" $ do
-      x <- archie
-      x `shouldBe` ()
+  describe "Selector" $ do
+    it "value defaults to 'inherit'" $ do
+      let x = value $ text $ Text.pack "#container"
+      x `shouldBe` value (Literal $ Text.pack "inherit")
 
-  -- example quickcheck test in hspec.
-  describe "read" $ do
-    it "is inverse to show" $ property $
-      \x -> (read . show) x == (x :: Int)
