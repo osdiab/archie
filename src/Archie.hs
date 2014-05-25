f919c432cbc120951aa9dcdf2b94f323bf37c385
Alexander Atallah <aatallah@stanford.edu>
2014-05-25 15:41:14 -0700

    initial type ideas

--- src/Archie.hs
+++ src/Archie.hs
@@ -1,4 +1,24 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving, TypeSynonymInstances, FlexibleInstances #-}
+
 module Archie (archie) where
+
+import Clay
+import qualified Data.Text as Text
+
+newtype Reference = Reference Selector
+  deriving (Show, Val)
+
+newtype JSValue = JSValue { unJSValue :: String }
+  deriving (Show)
+
+class JSVal a where
+  valueJS :: a -> JSValue
+
+instance Val Selector where
+  value a = value $ Literal $ Text.pack "inherit"
+
+instance JSVal Reference where
+  valueJS a = JSValue { unJSValue = show a ++ ".attr()" }
 
 archie :: IO ()
 archie = return ()
