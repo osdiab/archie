b2d2c807763f6e1adc9f3c51d16c1a5dfd169784
Omar Diab <o.s.diab@gmail.com>
2014-06-03 23:21:49 -0700

    add another css test, trivial

--- test/Test.hs
+++ test/Test.hs
@@ -15,3 +15,11 @@ main = hspec $ describe "Testing Archie" $ do
     it "Can generate empty CSS" $ do
       let sheet = Stylesheet []
       renderCss sheet `shouldBe` ""
+
+    it "Can generate a single style rule" $ do
+      let selector = Selector "#container"
+      let declaration = Declaration (Property "width") (DimensionVal 960 Px)
+      let ruleset = Ruleset (Just selector) (Declarations [declaration])
+      let statement = RulesetSt ruleset
+      let sheet = Stylesheet [statement]
+      renderCss sheet `shouldSatisfy` (\val -> length val /= 0)
